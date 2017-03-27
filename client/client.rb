2e81c0aab40c82d20abf2825a7741e3052ea9da3
Philip Ross <rosspa@rose-hulman.edu>
2017-03-27 04:20:02 -0400

    implmeneted iWant

--- client/client.rb
+++ client/client.rb
@@ -9,11 +9,13 @@ def input_loop
     socket = TCPSocket.new 'localhost', PORT
     trap('INT') { puts 'Shutting down.'; socket.close; exit }
 
-    send socket, input
-    handle_response(socket)
+    command, *args = parse_command input
+    socket.close and next unless validate_command(command, args)
+    send socket, input do
+      handle_response socket, command, args
+    end
     socket.close # close socket when done
     puts "closed socket\n\n"
-
   end
 end
 
@@ -21,14 +23,37 @@ def send(socket, input)
   input += "\0"
   puts "sending '#{input}'"
   socket.write(input)
+  yield
 end
 
-def handle_response(socket)
+def handle_response(socket, command, args)
   puts 'server response'
-  while line = socket.gets # Read lines from socket
-    puts line # and print them
+  response_code = socket.gets.to_i
+  unless response_code == 200
+    return puts "#{response_code} #{socket.gets}"
+  end
+  file_path = args.first
+  print "What directory would you like to store '#{file_path}' (default: default/): "
+  directory = gets.chomp
+  directory = "default" if directory.empty?
+  File.open(directory + '/' + file_path, 'w') do |file|
+    while line = socket.gets # Read lines from socket
+      file.puts(line)
+    end
   end
 end
 
+def parse_command(input)
+  input.split(" ")
+end
+
+def validate_command(command, args)
+  case command
+    when "uTake"
+      true
+    else
+      true
+  end
+end
 
 input_loop
--- server/server.rb
+++ server/server.rb
@@ -1,38 +1,82 @@
 require 'socket'
 
+Thread.abort_on_exception=true
+
 PORT = 3000
 CHUNK_SIZE = 4
 
-def main_loop
-  server = TCPServer.new PORT
-  trap('INT') { puts 'Shutting down.'; server.close; exit }
+class String
+  def underscore
+    gsub(/::/, '/').
+        gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
+        gsub(/([a-z\d])([A-Z])/, '\1_\2').
+        tr("-", "_").
+        downcase
+  end
+end
 
-  puts "server started on port #{PORT}"
-  loop do
-    Thread.start(server.accept) do |client|
-      handle_connection client
-      client.close
+class Server
+  def start
+    server = TCPServer.new PORT
+    trap('INT') { puts 'Shutting down.'; server.close; exit }
+
+    puts "server started on port #{PORT}"
+    loop do
+      Thread.start(server.accept) do |client|
+        handle_connection client
+        client.close
+      end
     end
   end
-end
 
-def handle_connection(client)
-  puts "\nnew connection"
-  input = parse_client_input client
-  puts "client sent '#{input}'"
-  client.puts 'Hello !'
-  client.puts "Time is #{Time.now}"
-end
-
-# TODO use read once we know how many bytes to read
-def parse_client_input(client)
-  data = ""
-  while tmp = client.recv(CHUNK_SIZE)
-    puts tmp
-    data += tmp
-    break if tmp.end_with? "\0"
+  def handle_connection(client)
+    puts "\nnew connection"
+    command, *args = parse_command read(client)
+    puts "client sent '#{command}' with '#{args}'"
+    if respond_to? command.to_sym
+      send(command, client, args)
+    else
+      puts "501 Command '#{command}' is not supported"
+      client.puts 501
+      client.puts "Command '#{command}' is not supported"
+    end
   end
-  data.chomp("\0")
+
+  def read(client)
+    data = ""
+    while tmp = client.recv(CHUNK_SIZE)
+      data += tmp
+      break if tmp.end_with? "\0"
+    end
+    data.chomp("\0")
+  end
+
+  def parse_command(input)
+    inputs = input.split(" ")
+    inputs[0] = inputs[0].underscore
+    inputs
+  end
+
+  def send_400(client)
+    puts "400 Bad Request"
+    client.puts(400)
+    client.puts("Bad Request")
+  end
+
+  def send_404(client, path)
+    puts "404 File Not Found at 'store/#{path}'"
+    client.puts(404)
+    client.puts("File Not Found at '#{path}'")
+  end
+
+  def i_want(client, args)
+    return send_400 client unless args.size == 1
+    path = "store/" + args.first
+    return send_404 client, path.gsub("store/", "") unless File.file?(path)
+    client.puts(200)
+    file = File.open(path, "rb")
+    client.puts file.read
+  end
 end
 
-main_loop
+Server.new.start
